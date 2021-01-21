159de178a2392be2ee29b3fce756aebc2ddff934
renan <48842827+humrenan@users.noreply.github.com>
2021-01-21 16:12:24 -0400

    Create If.js
    Exercício de adivinhar a estação do ano usando if / else if do Javascript

---
+++ Javascript/If.js
@@ -0,0 +1,21 @@
+
+var mes = "setembro"
+if(mes == "janeiro" || mes == "fevereiro" ||mes == "março"){
+    estacao = " é verão"
+    console.log(mes + estacao)
+}
+else if(mes == "abril" || mes == "maio" || mes == "junho"){
+    estacao = " é outono"
+    console.log(mes + estacao)
+}
+else if(mes == "julho" || mes == "agosto" || mes == "setembro"){
+    estacao = " é inverno"
+    console.log(mes + estacao)
+}
+else if(mes == "julho" || mes == "agosto" || mes == "setembro"){
+    estacao = " é primavera"
+    console.log(mes + estacao)
+}
+else{
+    console.log("inválido ")
+}
