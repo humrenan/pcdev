cd72e003807b55c0e0cebb390fbb3acf7e14a426
renan <48842827+humrenan@users.noreply.github.com>
2021-01-21 16:09:41 -0400

    Create switch.js
    Exercício usando Switch do Javascript

---
+++ Javascript/switch.js
@@ -0,0 +1,71 @@
+var mes = "abril"
+var estacao
+
+
+switch(mes){
+    case "janeiro":
+        estacao = "verao"
+        console.log(estacao)
+    break;
+
+   
+    case "fevereiro":
+        estacao = "verao"
+        console.log(estacao)
+    break;
+        
+    case "março": 
+    estacao = "verao"
+    console.log(estacao)
+    break;
+
+   
+    case "abril":
+        estacao = "outono"
+        console.log(estacao)
+    break;
+        
+    case "maio":
+        estacao = "outono"
+        console.log(estacao)
+    break;
+        
+    case "junho": 
+    estacao = "outono"
+    console.log(estacao)
+    break;
+
+   
+    case "julho":
+        estacao = "inverno"
+        console.log(estacao)
+    break;
+        
+    case "agosto":
+        estacao = "inverno"
+        console.log(estacao)
+    break;
+        
+    case "setembro": 
+    estacao = "inverno"
+    console.log(estacao)
+    break;
+
+   
+    case "outubro":
+        estacao = "primavera"
+        console.log(estacao)
+    break;
+        
+    case "novembro":
+        estacao = "primavera"
+        console.log(estacao)
+    break;
+        
+    case "dezembro": 
+    estacao = "primavera"
+    console.log(estacao)
+    break;
+
+
+}
